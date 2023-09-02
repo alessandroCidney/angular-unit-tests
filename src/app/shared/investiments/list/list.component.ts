713535c111bea1063e3988d29e419d9a89ee9386
Alessandro Cídney <contatoalessandrocidneysa@gmail.com>
2023-09-02 19:19:48 -0300

    Created investiments component

---
+++ src/app/shared/investiments/list/list.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Investiments } from '../model/investiments'
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+export class ListComponent {
+  public investiments: Array<Investiments> = [
+    {
+      name: 'Bank 1',
+      value: 100
+    },
+    {
+      name: 'Bank 2',
+      value: 100
+    },
+    {
+      name: 'Bank 3',
+      value: 100
+    },
+    {
+      name: 'Bank 4',
+      value: 100
+    },
+  ]
+}
