9ead857b170b46e2a7f75f8d3eed3bd7fc32f6d5
Alessandro Cídney <contatoalessandrocidneysa@gmail.com>
2023-09-02 19:44:28 -0300

    Testing investiments component

---
+++ src/app/shared/investiments/components/list/list.component.spec.ts
@@ -0,0 +1,36 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListComponent } from './list.component';
+
+describe('ListComponent', () => {
+  let component: ListComponent;
+  let fixture: ComponentFixture<ListComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ListComponent]
+    });
+    fixture = TestBed.createComponent(ListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('(U) should list investiments', () => {
+    expect(component.investiments.length).toEqual(4)
+    expect(component.investiments[0].name).toEqual('Bank 1')
+    expect(component.investiments[3].name).toEqual('Bank 4')
+  })
+
+  it('(I) should list investiments', () => {
+    const investiments: HTMLElement[] = fixture.debugElement.nativeElement
+      .querySelectorAll('.list-item')
+
+    expect(investiments.length).toEqual(4)
+    expect(investiments[0].textContent?.trim()).toEqual('Bank 1 | 100')
+    expect(investiments[3].textContent?.trim()).toEqual('Bank 4 | 100')
+  })
+});
